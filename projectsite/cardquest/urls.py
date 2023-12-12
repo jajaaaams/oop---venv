d461337bdff7d85d34375361f4768c409a4fff52
Laurence <120358065+jajaaaams@users.noreply.github.com>
2023-12-12 22:44:47 +0800

    Delete projectsite directory

--- projectsite/cardquest/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
-    path('trainer', views.TrainerList.as_view(), name='trainer-list'),
-    path('pokemon_card', views.PokemonCardList.as_view(), name='pokemon-card'),
-    path('collection', views.CollectionList.as_view(), name='collection'),
-]
