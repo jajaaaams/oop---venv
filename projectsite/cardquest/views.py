6a29956310e53e74a675f9c1b22e6a26243e0b47
Laurence <120358065+jajaaaams@users.noreply.github.com>
2023-12-14 21:53:54 +0800

    Add files via upload

---
+++ projectsite/cardquest/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from cardquest.models import Trainer, PokemonCard, Collection
+from cardquest.forms import TrainerForm
+from django.urls import reverse_lazy
+import json
+
+# Create your views here.
+class HomePageView(ListView):
+    model = PokemonCard
+    context_object_name = 'home'
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+class TrainerList(ListView):
+    model = Trainer
+    context_object_name = 'trainer'
+    template_name = 'trainer.html'
+    paginate_by = 9
+
+class CollectionList(ListView):
+    model = Collection
+    context_object_name = 'collection'
+    template_name = 'collection.html'
+    paginate_by = 10
+
+class TrainerCreateView(CreateView):
+    model = Trainer
+    form_class = TrainerForm
+    template_name = 'trainer_add.html'
+    success_url = reverse_lazy('trainer-list')
+
+class TrainerUpdateView(UpdateView):
+    model = Trainer
+    form_class = TrainerForm
+    template_name = 'trainer_edit.html'
+    success_url = reverse_lazy('trainer-list')
+
+class TrainerDeleteView(DeleteView):
+    model = Trainer
+    template_name = 'trainer_del.html'
+    success_url = reverse_lazy('trainer-list')
+
+class PokemonCardListView(ListView):
+    model = PokemonCard
+    context_object_name = 'pokemoncard'
+    template_name = "pokemon-card.html"
+    json_file_path = 'data/pokemon_data.json'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pokemon_data = self.get_pokemon_data()
+        context['pokemon_data'] = pokemon_data
+        return context
+    
+    def get_pokemon_data(self):
+        with open(self.json_file_path, 'r') as file:
+            data = json.load(file)
+            return data.get('pokemons', [])
+
