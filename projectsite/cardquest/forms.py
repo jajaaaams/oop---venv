dec0173a541d6992982bb4aebf582ed010e6f472
Laurence <120358065+jajaaaams@users.noreply.github.com>
2023-12-14 21:52:35 +0800

    Delete projectsite directory

--- projectsite/cardquest/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from django import forms
-from .models import Trainer, Collection, PokemonCard
-
-class TrainerForm(ModelForm):
-    birthdate = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-    class Meta:
-        model = Trainer
-        fields = "__all__"
